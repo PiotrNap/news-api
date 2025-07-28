401bee25dddd3257959eccbd111699bbe7356f1b
Piotr Napierala <piotr.napierala94@gmail.com>
2025-07-28 17:30:15 +0200

    add LRA database class and wip server endpoints

--- db.js
+++ db.js
@@ -5,7 +5,40 @@
 
 class LRACache {
   constructor() {
-    this.db = [];
+    this.db = new Map();
+    this.maxLength = 100;
+  }
+
+  set(key, value) {
+    // check if it already exists
+    if (this.db.has(key)) {
+      // if it does then remove the current one,
+      this.db.delete(key);
+    } else {
+      const currLength = this.db.size;
+      if (currLength === 100) {
+        const lastKey = this.db.keys().next().value;
+        this.db.delete(lastKey);
+      }
+    }
+
+    // and add the fresh one.
+    this.db.set(key, value);
+  }
+
+  get(key) {
+    // check if it exists, if not return null
+    if (!this.db.get(key)) {
+      return null;
+    }
+
+    const val = this.db.get(key);
+
+    //delete it, and put it at the top of the stack
+    delete this.db.delete(key);
+    this.set(key, val);
+
+    return val;
   }
 }
 
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const limiter = ratelimit({
   limit: 100,
 });
 
-const API_URL = "https://gnews.io/api/v4/";
+const API_URL = "https://gnews.io/api/v4/search";
 const PORT = 9000;
 const HOST = "localhost";
 const app = new Express();
@@ -18,41 +18,77 @@ const app = new Express();
 /**
  * Helper functions
  */
-function getAPIUrl(endpoint = "") {
-  return API_URL + endpoint + `?apiKey=${NEWS_API_KEY}`;
+function getAPIUrl(query = "") {
+  return API_URL + query + `&apiKey=${NEWS_API_KEY}`;
 }
 
-app.use(limiter);
 app.use(Express.json());
 
-/**
- * Get the latest news
- */
-app.get("/", async (req, res, next) => {
+app.get("/help", async (req, res, next) => {
+  const message = `To interact with our server, you can use the below endpoints:
+
+    1. GET ${HOST}:${PORT} - to preview this message
+    4. GET ${HOST}:${PORT}?q=[search-query] - to get articles searched by a given query
+    2. GET ${HOST}:${PORT}?q=[search-query]&max=[N] - to get the N max news (1-100), by a given search query (q)
+    3. GET ${HOST}:${PORT}?q=[search-query]&in=[title] - to get articles searched by a given title
+
+    Good luck!
+    `;
+
+  res.send(message);
+});
+
+app.get("/news", limiter, async (req, res, next) => {
+  const query = req.query;
+
+  const { q, max, in: searchIn } = query;
+
+  if (!q) {
+    res
+      .status(400)
+      .send(
+        "missing required query param 'q'. Please call /help endpoint to learn more",
+      );
+    next();
+  }
+
+  if (max & searchIn) {
+    const recordKey = max + "_" + searchIn;
+    let result = this.db.get(recordKey);
+    if (result) {
+      res.send(result);
+      next();
+    }
+
+    try {
+      const res = await fetch(getAPIUrl());
+      console.log(res);
+      if (!res.ok) {
+        res.status(500).send("We were unable to obtain latest news");
+        console.error(res.body);
+      }
+      const data = await res.json();
+
+      res.send(data);
+    } catch (e) {
+      res.status(500).send("We were unable to obtain latest news");
+    }
+  }
+
+  return;
   try {
     const res = await fetch(getAPIUrl());
+    console.log(res);
     if (!res.ok) {
-      res.status(500).message("We were unable to obtain latest news");
+      res.status(500).send("We were unable to obtain latest news");
       console.error(res.body);
     }
     const data = await res.json();
 
     res.send(data);
   } catch (e) {
-    res.status(500).message("We were unable to obtain latest news");
+    res.status(500).send("We were unable to obtain latest news");
   }
 });
-
-/**
- * fetching N news articles
- */
-
-/**
- * finding a news article with a specific title or author
- */
-
-/**
- * searching by keywords
- */
 
 app.listen(PORT, HOST, () => console.log("News are ready to be delivered"));
