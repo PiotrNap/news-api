13a19a3dd2c3dce21ea178958bb77f34dfddbacd
Piotr Napierala <piotr.napierala94@gmail.com>
2025-07-28 16:39:13 +0200

    add basic setup and first handler

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 import Express from "express";
-import RateLimit from "express-rate-limit";
+import ratelimit from "express-rate-limit";
+import fetch from "node-fetch";
 
-import { NEWS_API_KEY } from "./env";
-import { db } from "./db";
+import { NEWS_API_KEY } from "./env.js";
+import { db } from "./db.js";
 
-const limiter = new RateLimit({
+const limiter = ratelimit({
   windowMs: 15 * 60 * 1000,
   limit: 100,
 });
 
+const API_URL = "https://gnews.io/api/v4/";
+const PORT = 9000;
+const HOST = "localhost";
 const app = new Express();
 
+/**
+ * Helper functions
+ */
+function getAPIUrl(endpoint = "") {
+  return API_URL + endpoint + `?apiKey=${NEWS_API_KEY}`;
+}
+
 app.use(limiter);
-app.ise(Express.json());
+app.use(Express.json());
 
 /**
- * Get lates news
+ * Get the latest news
  */
+app.get("/", async (req, res, next) => {
+  try {
+    const res = await fetch(getAPIUrl());
+    if (!res.ok) {
+      res.status(500).message("We were unable to obtain latest news");
+      console.error(res.body);
+    }
+    const data = await res.json();
+
+    res.send(data);
+  } catch (e) {
+    res.status(500).message("We were unable to obtain latest news");
+  }
+});
+
+/**
+ * fetching N news articles
+ */
+
+/**
+ * finding a news article with a specific title or author
+ */
+
+/**
+ * searching by keywords
+ */
+
+app.listen(PORT, HOST, () => console.log("News are ready to be delivered"));
